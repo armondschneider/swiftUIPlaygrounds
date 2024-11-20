e5f21110c860d742ae85391e80c9e4f361f7784c
Armond Schneider <mondoloves@icloud.com>
2024-11-20 14:22:42 -0700

    Initial Commit

---
+++ swiftUIPlaygrounds/swiftUIPlaygroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPlaygroundsApp.swift
+//  swiftUIPlaygrounds
+//
+//  Created by Armond Schneider on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPlaygroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
